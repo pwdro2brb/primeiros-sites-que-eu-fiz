9cc6176eea7c836d67b1d809bba4b6ff03253105
Pedro Henrique Soares <103432991+pwdro2brb@users.noreply.github.com>
2022-08-14 02:50:35 +0000

    concertei os sites e adicionei estilos decentes neles

---
+++ script2.js
@@ -0,0 +1,14 @@
+let x = 80 //modifique o valor aqui para ver no console quando o código funcionar
+function primo(){
+for(let i = 0; i <= x; i++){
+let y = x % i;
+let t =+ x
+if (y !== 0){
+for(let i = 1; i <= x; i++){
+}console.log()
+}else{
+console.log(i)
+}
+}
+}
+console.log(primo())
